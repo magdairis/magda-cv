8652d78486c876837872ee4753eae41b2f5a061b
magdairis <magdapetford@gmail.com>
2020-07-20 08:41:46 +0100

    init

---
+++ components/Address.js
@@ -0,0 +1,31 @@
+export default function Address() {
+  return (
+    <address>
+      <ul>
+        <li>
+          <a href="https://bearjam.dev/about" class="website">
+            Bio
+          </a>
+        </li>
+        <li>
+          <a href="mailto:hello@magdairis.com" class="email">
+            hello@magdairis.com
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/magdairispetford/"
+            class="linkedin"
+          >
+            Magda Iris Petford
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/bymagdairis" class="instagram">
+            @bymagdairis
+          </a>
+        </li>
+      </ul>
+    </address>
+  )
+}
